457903a734bab568b39d9085188056e195bc24a6
Stas Maksimov <maksimov@gmail.com>
2015-10-30 15:17:01 +0000

    Fixing the merge errors

--- src/main/java/org/dasein/cloud/cloudstack/compute/VMCapabilities.java
+++ src/main/java/org/dasein/cloud/cloudstack/compute/VMCapabilities.java
@@ -152,6 +152,12 @@ public class VMCapabilities extends AbstractCapabilities<CSCloud> implements Vir
 
     @Nonnull
     @Override
+    public String[] getVirtualMachineReservedUserNames() {
+        return new String[0];
+    }
+
+    @Nonnull
+    @Override
     public Requirement identifyDataCenterLaunchRequirement() throws CloudException, InternalException {
         return Requirement.REQUIRED;
     }
